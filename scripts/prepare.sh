571379abc8fbe3a1f1c5d17775b3ef65fa6a0e6e
Jun Tsai <jcai@ganshane.com>
2014-10-10 11:24:53 +0800

    尝试减少日志信息

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,21 +1,24 @@
 mkdir -p target
 cd target
 #install boost
+echo "Compling boost ...."
 wget "http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2" -O /tmp/boost.tar.bz2
 tar xfj /tmp/boost.tar.bz2
 cd boost*
-sudo ./b2 cxxflags=-fPIC  --with-thread --with-system link=static threading=multi  install
+sudo ./b2 cxxflags=-fPIC  --with-thread --with-system link=static threading=multi  install > /tmp/install.log
 cd -
 #zeromq 
+echo "Compling zmq ...."
 wget "http://download.zeromq.org/zeromq-3.2.4.tar.gz" -O /tmp/zmq.tar.gz
 tar xfz /tmp/zmq.tar.gz
 cd zeromq*
 ./configure --with-pic
-make
-sudo make install
+make > /tmp/make.log
+sudo make install > /tmp/install.log
 cd -
 
 #install snappy
+echo "Compling snappy ...."
 git clone https://github.com/google/snappy.git
 cd snappy
 ./autogen.sh
