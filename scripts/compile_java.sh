c6202626671a787818be09a10596f343d841315c
Jun Tsai <jcai@ganshane.com>
2014-10-10 13:57:17 +0800

    尝试修正编译问题

--- scripts/compile_java.sh
+++ scripts/compile_java.sh
@@ -1,7 +1,7 @@
 cd target/monad
 BUILD_NUMBER=r`git log -n1 --pretty=format:%at | awk '{print strftime("%y%m%d%H%M",$1)}'`
 if [ $TRAVIS_TAG ];then
-  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
+  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=$BUILD_ID -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
 else
-  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean compile
+  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=$BUILD_ID -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
 fi
